7d9dee81c175765e2cc74520ea6526fb3ff67562
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-06-18 10:37:43 -0400

    customized schema file path

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 // external packages
+var fs = require('fs');
+var debug = require('debug')('dataset:index');
 var MongoClient = require('mongodb').MongoClient;
 var argv = require('minimist')(process.argv.slice(2));
 // internal packages
+var Inserter = require('./inserter');
 var Generator = require('./generator');
 var schemaBuilder = require('./schema')();
-var Inserter = require('./inserter');
-var debug = require('debug')('dataset:index');
 
 // assume these are the user input
 var serverName = 'mongodb://127.0.0.1:27017/',
     dbName = 'test',
-    datasetName = 'dataset',
-    size = argv.size,
-    rawSchema = {
-			first_name: 'first',
-			last_name: 'last',
-			email: 'email'
-		};
-
+    datasetName = 'dataset';
+var size = argv.size;
+var schemaFile = __dirname + '/' + argv.schema;
 // to build a Schema object from user input
-var schema = schemaBuilder.build(rawSchema);
-
-// get the customized iterator to generate random data
-var dataStream = new Generator(schema, size);
-
-// connect to MongoDB
-MongoClient.connect(serverName + dbName, function(err, db) {
-  debug('INFO: connected to MongoDB');
-  if(err) throw err;
-
-  // the collection to dump the generated data
-  var collection = db.collection(datasetName);
-
-  // initiate the inserter to do the job
-  var inserter = new Inserter(collection, dataStream, function() {
-  	console.log('This is the callback function user defined');
-    console.log('Yay! We reached here.');
-    db.close();
-  });
-
-  inserter.start();
-
+var schema, dataStream;
+fs.readFile(schemaFile, 'utf8', function (err, data) {
+  debug('Schema file path: %j', schemaFile);
+  if (err) {
+    throw err;
+    debug('No schema file is specified. Using default.');
+    schema = { first_name: 'first',
+               last_name: 'last',
+               user_email: 'email',
+               cell_phone: 'phone',
+               birthday: 'birthday',
+               description: 'sentence'
+             };
+  } else {
+    schema = schemaBuilder.build(JSON.parse(data));
+  }
+  debug('Schema built as %j', schema);
+  dataStream = new Generator(schema, size);
+  main();
 });
 
+  // connect to MongoDB
+var main = function () {
+  MongoClient.connect(serverName + dbName, function(err, db) {
+    debug('INFO: connected to MongoDB');
+    if(err) throw err;
+    // the collection to dump the generated data
+    var collection = db.collection(datasetName);
+    // initiate the inserter to do the job
+    var inserter = new Inserter(collection, dataStream, function() {
+    	console.log('This is the callback function user defined');
+      console.log('Yay! We reached here.');
+      db.close();
+    });
+    // start the inserter
+    inserter.start();
+  });
+};
