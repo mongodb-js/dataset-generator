4bf07273f8a2e787c64f123401579014d00a80a0
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-06-18 16:56:51 -0400

    add test empty schema

--- index.js
+++ index.js
@@ -2,52 +2,56 @@
 var fs = require('fs');
 var debug = require('debug')('dataset:index');
 var MongoClient = require('mongodb').MongoClient;
-var argv = require('minimist')(process.argv.slice(2));
+// var argv = require('minimist')(process.argv.slice(2));
 // internal packages
 var Inserter = require('./inserter');
 var Generator = require('./generator');
 var schemaBuilder = require('./schema')();
 
 // assume these are the user input
-argv.serverName = 'mongodb://127.0.0.1:27017/';
-argv.dbName = 'test';
-argv.datasetName = 'dataset';
-argv.schemaFile = __dirname + '/' + argv.schema;
-// to build a Schema object from user input
-var schema, dataStream;
-fs.readFile(argv.schemaFile, 'utf8', function (err, data) {
-  debug('Schema file path: %j', argv.schemaFile);
-  if (err) {
-    debug('No schema file is specified. Using default.');
-    schema = { first_name: 'first',
-               last_name: 'last',
-               user_email: 'email',
-               cell_phone: 'phone',
-               birthday: 'birthday',
-               description: 'sentence'
-             };
-  } else {
-    schema = schemaBuilder.build(JSON.parse(data));
-  }
-  debug('Schema built as %j', schema);
-  dataStream = new Generator(schema, argv.size);
-  main();
-});
+var main = function (opt, callback) {
+  opt.size = opt.size || 100;
+  opt.serverName = opt.serverName || 'mongodb://127.0.0.1:27017/';
+  opt.dbName = opt.dbName || 'test';
+  opt.datasetName = opt.datasetName || 'dataset';
+  opt.schemaFile = opt.schema ? (__dirname + '/' + opt.schema) : '';
 
-// connect to MongoDB
-var main = function () {
-  MongoClient.connect(argv.serverName + argv.dbName, function(err, db) {
-    debug('INFO: connected to MongoDB');
-    if(err) throw err;
-    // the collection to dump the generated data
-    var collection = db.collection(argv.datasetName);
-    // initiate the inserter to do the job
-    var inserter = new Inserter(collection, dataStream, function() {
-    	console.log('This is the callback function user defined');
-      console.log('Yay! We reached here.');
-      db.close();
+  // to build a Schema object from user input
+  var schema, dataStream;
+  fs.readFile(opt.schemaFile, 'utf8', function (err, data) {
+    debug('Schema file path: %j', opt.schemaFile);
+    if (err) {
+      debug('No schema file is specified. Using default.');
+      schema = { first_name: 'first',
+                 last_name: 'last',
+                 user_email: 'email',
+                 cell_phone: 'phone',
+                 birthday: 'birthday',
+                 description: 'sentence'
+               };
+    } else {
+      schema = schemaBuilder.build(JSON.parse(data));
+    }
+    debug('Schema built as %j', schema);
+    dataStream = new Generator(schema, opt.size);
+
+    // core
+    MongoClient.connect(opt.serverName + opt.dbName, function(err, db) {
+      debug('INFO: connected to MongoDB');
+      if(err) throw err;
+      // the collection to dump the generated data
+      var collection = db.collection(opt.datasetName);
+      // initiate the inserter to do the job
+      var inserter = new Inserter(collection, dataStream, function() {
+      	console.log('This is the callback function user defined');
+        console.log('Yay! We reached here.');
+        callback();
+        db.close();
+      });
+      // start the inserter
+      inserter.start();
     });
-    // start the inserter
-    inserter.start();
   });
 };
+
+module.exports = main;
---
+++ test/basic.js
@@ -0,0 +1,68 @@
+var assert = require('assert');
+var populator = require('../index.js');
+
+describe('Populator', function () {
+	var MongoClient = require('mongodb').MongoClient;
+  var myCollection, myDb;
+
+	var options = {
+    serverName: 'mongodb://127.0.0.1:27017/',
+    dbName: 'test',
+    datasetName: 'dataset',
+		schema: 'test/schema_basic.json',
+		size: 111
+	};
+
+  before(function(done) {
+  	MongoClient.connect(options.serverName + options.dbName, function(err, db) {
+  		if (err) return done(err);
+  		myDb = db;
+  		myCollection = db.collection(options.datasetName);
+  		myCollection.remove({}, {safe: true}, function (err, res) {
+  			if (err) done(err);
+	  		populator(options, function () { done(); });
+  		});
+  	});
+  });
+
+  after(function(done) {
+  	myCollection.drop();
+  	myDb.close();
+  	done();
+  });
+
+	it('should have the correct size', function (done) {
+		myCollection.count(function (err, count) {
+			if (err) done(err);
+			assert.equal(count, options.size);
+			done();
+		});
+	});
+
+	// it('entries should conform to the schema', function (done) {
+	// 	var cursor = myCollection.find();
+	// 	cursor.each(function (err, item) {
+	// 		if (err) done(err);
+	// 		if (item === null) {
+	// 			done();
+	// 		} else {
+	// 			var keys = Object.keys(item);
+	// 			assert(keys.length, 1);
+	// 		}
+	// 	});
+	// });
+
+	// Schema:
+	// {
+	//   "username": "name",
+	//   "email": "email"
+	// }
+
+	// it('entries should conform to the schema', function (done) {
+	// 	myCollection.findOne({}, function (err, doc) {
+	// 		if (err) done(err);
+
+	// 	});
+	// });
+
+});
