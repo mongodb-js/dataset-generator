0468bfdc12b2ad1263cc986c0d96769e2b3223c2
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-06-30 17:04:38 -0400

    rename dbUtil with helper

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var debug = require('debug')('dataset:index');
 var Inserter = require('./inserter');
-var util = require('./dbUtil');
+var helper = require('./helper');
 var Generator = require('./generator');
 var schemaBuilder = require('./schema')();
 
 // assume these are the user input
 module.exports = function (opts, fn) {
-  util.parseUserOpts(opts, function (opts) {
+  helper.parseUserOpts(opts, function (opts) {
     var schema = schemaBuilder.build(opts.schema);
     var dataStream = new Generator(schema, opts.size);
-    util.connect(opts, function(collection, db) {
+    helper.connect(opts, function(collection, db) {
       var inserter = new Inserter(collection, dataStream, function() {
         fn();
         db.close();
