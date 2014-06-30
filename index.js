fbc171b82e43e74e448eb73c3e906c1554eb0cee
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-06-30 11:28:05 -0400

    index.js uses the new method

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var debug = require('debug')('dataset:index');
 var Inserter = require('./inserter');
 var util = require('./dbUtil');
+var Generator = require('./generator');
+var schemaBuilder = require('./schema')();
 
 // assume these are the user input
 module.exports = function (opts, fn) {
-  var user = util.parseInput(opts);
-  util.readSchema(user, function(schema, dataStream) {
-    util.connect(user, function(collection, db) {
+  util.parseUserOpts(opts, function (opts) {
+    var schema = schemaBuilder.build(opts.schema);
+    var dataStream = new Generator(schema, opts.size);
+    util.connect(opts, function(collection, db) {
       var inserter = new Inserter(collection, dataStream, function() {
         fn();
         db.close();
