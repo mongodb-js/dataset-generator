1d8582f2468998cd30420993aaa02a9ea9811ae9
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-01 15:31:18 -0400

    move chanceBuilder inside Schema.js

--- schema.js
+++ schema.js
@@ -1,5 +1,7 @@
 /* schema.js */
-var chanceBuilder = require('./chanceBuilder');
+var Chance = require('chance');
+var debug = require('debug')('dataset:schema');
+var chance;
 
 /**
  * A representation of schema specified by user that records the name and
@@ -22,7 +24,63 @@ function Schema (s) {
   if (!(this instanceof Schema)) return new Schema(s);
 
   this._schema = s;
-  this.chance = chanceBuilder(s);
+  this.chance = buildMixin(s);
+}
+
+/**
+ * A utility method to build the mixin needed for the input schema
+ */
+function buildMixin (schema) {
+  debug('Start building schema -> %j', schema);
+  chance = new Chance();
+  buildMixinHelper(schema, 0);
+  return chance;
+}
+
+function buildMixinHelper (schema, id) {
+  debug('id=%d starts building, schema -> %j', id, schema);
+  var treeSize = 1; // count itself
+  var flatSchema = {};
+  var field, type;
+  var simplifyField = function (type) {
+    if (typeof type !== 'object') {
+      return type;
+    } else {
+      var childId = id + treeSize;
+      treeSize += buildMixinHelper(type, childId);
+      return '_' + childId;
+    }
+  };
+  for (field in schema) {
+    type = schema[field];
+    if (type instanceof Array) {
+      flatSchema[field] = [simplifyField(type[0])];
+    } else {
+      flatSchema[field] = simplifyField(type);
+    }
+  }
+  debug('id=%d, flatSchema -> %j', id, flatSchema);
+  // add new mixin corresponding to schema
+  var mixin = {};
+  mixin['_' + id] = function () {
+    var o = {};
+    var field, type;
+    for (field in flatSchema) {
+      type = flatSchema[field];
+      // calling chance must happen inside this function
+      if (type instanceof Array) {
+        o[field] = [];
+        for (var i = 0; i < chance.d6(); i++) {
+          o[field].push(chance[type]());
+        }
+      } else {
+        o[field] = chance[type]();
+      }
+    }
+    return o;
+  };
+  chance.mixin(mixin);
+  return treeSize;
 }
 
 module.exports = Schema;
