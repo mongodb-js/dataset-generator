61413eed5ef4b1db9015362780675ad136a30d97
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-02 12:06:03 -0400

    emit structurally correct

--- sc.js
+++ sc.js
@@ -3,7 +3,6 @@ var debug = require('debug')('dataset:schema');
 var _ = require('underscore');
 var chance;
 _.templateSettings = {
-  variable: 'data',
   interpolate: /\{\{(.+?)\}\}/g
 };
 
@@ -11,6 +10,10 @@ function Schema (sc) {
   if (!(this instanceof Schema)) return new Schema(sc);
   this._schema = new Document(sc);
 }
+
+Schema.prototype.emit = function () {
+  return this._schema.emit();
+};
 
 // doc must be an object or an array of object
 function Document (document) {
@@ -30,6 +33,26 @@ function Document (document) {
   }
 }
 
+Document.prototype._produce = function () {
+  var data = {};
+  for (var name in this._document) {
+    data[name] = this._document[name].emit();
+  }
+  return data;
+};
+
+Document.prototype.emit = function () {
+  if (this._array) {
+    var data = [];
+    for (var i = _.random(1, 3); i > 0; i--) {
+      data.push(this._produce());
+    }
+    return data;
+  } else {
+    return this._produce();
+  }
+};
+
 // f must be string or an array of string
 function Field (f) {
   if (!(this instanceof Field)) return new Field(f);
@@ -40,6 +63,25 @@ function Field (f) {
     this._array = true;
     this._field = f[0];
   }
+  this._compiled = _.template(this._field);
 }
+
+Field.prototype._produce = function () {
+  return this._compiled({});
+};
+
+Field.prototype.emit = function () {
+  if (this._array) {
+    var data = [];
+    for (var i = _.random(1, 3); i > 0; i--) {
+      data.push(this._produce());
+    }
+    return data;
+  } else {
+    return this._produce();
+  }
+};
+
+var raw = { field: 'string', obj: { field: 'string' }, ar: ['string'], arobj: [{ field: 'string' }]}
 
 module.exports = Schema;
