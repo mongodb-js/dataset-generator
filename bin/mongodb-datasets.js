1b96f344731318f341eaac36c2be1ff6bfd9c183
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-18 18:12:35 -0400

    updated bin

--- bin/mongodb-datasets.js
+++ bin/mongodb-datasets.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-var main = require('../');
+var md = require('../');
 var argv = require('minimist')(process.argv.slice(2));
+var es = require('event-stream');
+var fs = require('fs');
 
 console.log(argv);
 
-main.populate(argv, function () {
-  console.log('done');
-});
+fs.createReadStream('../examples/me_in_a_nutshell.json')
+  .pipe(md.createGeneratorStream(argv))
+  .pipe(es.map(function (data, callback) {
+    callback(JSON.stringify(data, null, 2));
+  }))
+  .pipe(process.stdout);
+
+// fs.createReadStream('./examples/me_in_a_nutshell.json')
+//   .pipe(md.createGeneratorStream(argv))
+//   .pipe(md.createPopulatorStream(argv));
