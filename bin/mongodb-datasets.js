cf6c54495ef41dadf2c60fcc35b18ab71f190853
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-21 17:02:05 -0400

    better help msg

--- bin/mongodb-datasets.js
+++ bin/mongodb-datasets.js
@@ -5,8 +5,8 @@ var es = require('event-stream');
 var fs = require('fs');
 var yargs = require('yargs')
       .usage('MongoDB-Datasets version 0.0.0')
-      .example('$0 schema.json -n 10', '')
-      .example('cat schema.json | $0 -n 10', '')
+      .example('$0 schema.json -n 10 -o dump.out', '')
+      .example('cat schema.json | $0 -n 10 -o -', '')
       .describe(0, 'Path to a template schema file')
       .options('n', {
         demand: true,
@@ -31,18 +31,19 @@ var yargs = require('yargs')
       .check(function (argv) {
         if (argv._.length > 1)
           throw 'Too many arguments!';
-        if (argv._[0] && !fs.existsSync(argv._[0]))
+        var _0 = argv._[0];
+        if (_0 && _0 !== 'help' && !fs.existsSync(_0))
           throw 'Schema file does not exist!';
       });
 var argv = yargs.argv;
 
-if (argv.h) return yargs.showHelp();
+if (argv.h || argv._[0] === 'help') return yargs.showHelp();
 
 (argv._[0] ? fs.createReadStream(argv._[0]) : process.stdin)
   .pipe(datasets.createGeneratorStream({size: argv.size}))
   .pipe(es.map(function (data, callback) {
     if (argv.pretty) {
-      callback(null, JSON.stringify(data, null, 2));
+      callback(null, JSON.stringify(data, null, 2) + '\n');
     } else {
       callback(null, JSON.stringify(data));
     }
