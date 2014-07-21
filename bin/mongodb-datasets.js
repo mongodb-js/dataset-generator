547d37bdd0abd3c366266c9956928df2b140ff0e
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-21 13:56:26 -0400

    remove mongodb dependent

--- bin/mongodb-datasets.js
+++ bin/mongodb-datasets.js
@@ -5,21 +5,16 @@ var helpers = require('../lib/helpers');
 var argv = require('minimist')(process.argv.slice(2));
 var es = require('event-stream');
 var fs = require('fs');
-var stream = require('stream');
 
 var myOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.cli);
 var genOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.generator);
-var popOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.populator);
 
 // console.log('All commands: ', argv);
 // console.log('For this script: ', myOpts);
 // console.log('For Generator: ', genOpts);
-// console.log('For Populator: ', popOpts);
 
 (myOpts.path ? fs.createReadStream(myOpts.path) : process.stdin)
   .pipe(md.createGeneratorStream(genOpts))
-  .pipe(myOpts.populate ? md.createPopulatorStream(popOpts)
-                        : new stream.PassThrough({objectMode: true}))
   .pipe(es.map(function (data, callback) {
     if (myOpts.raw) {
       callback(null, JSON.stringify(data));
@@ -28,5 +23,3 @@ var popOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.populator);
     }
   }))
   .pipe(process.stdout);
-  // .pipe(myOpts.display ? process.stdout
-                       // : new stream.PassThrough({objectMode: true}));
--- test/helpers.js
+++ test/helpers.js
@@ -4,83 +4,15 @@
 
 var chance = require('chance').Chance();
 var md = require('../');
-var dbUtil = require('../lib/helpers');
 var async = require('async');
-var MongoClient = require('mongodb').MongoClient;
-var debug = require('debug')('dataset:testUtil');
-var _ = require('underscore');
 var es = require('event-stream');
 var fs = require('fs');
 var path = require('path');
 var stream = require('stream');
 
-var defaults = {
-  host: 'localhost',
-  port: '27017',
-  db: 'test',
-  collection: 'dataset'
-};
-
-var regex = {
+module.exports.regex = {
   phone: /(\(\d{3}\)\s*)(\d{3})-(\d{4})/,
   exp: /(\d{2})\/(\d{4})/
-};
-
-// close the connection, drop the test collection
-function tearDown (connection, fn) {
-  connection.collection.drop();
-  connection.db.close();
-  if (typeof fn === 'function') fn();
-}
-
-// connects to the target collection, and possibly clear its content
-function setUp (testOpts, callback) {
-  var opts = _.defaults(testOpts, defaults);
-  dbUtil.parseUserOpts(opts, function (opts) {
-    MongoClient.connect(opts.uri, opts.clientOptions, function(err, db) {
-      debug('INFO: connected to MongoDB @ ', opts.uri);
-      if(err) return callback(err);
-      var collection = db.collection(opts.collection);
-      collection.remove({}, function(err, res) {
-        if(err) return callback(err);
-        md.populate(opts, function () {
-          var connection = {
-            db: db,
-            collection: collection
-          };
-          callback(null, connection);
-        });
-      });
-    });
-  });
-}
-
-function getResults (testOpts, callback) {
-  setUp(testOpts, function (err, connection) {
-    if (err) return callback(err);
-    connection.collection.find().toArray(function (err, items) {
-      if (err) return callback(err);
-      callback(null, items);
-      tearDown(connection);
-    });
-  });
-}
-
-function sampleAndStrip(array, count, fn) {
-  var sample = chance.pick(array, count);
-  async.each(sample,
-    function (item, callback) {
-      item._id = undefined;
-      callback();
-    },
-    function (err) {
-      if (err) throw err;
-      fn(sample);
-  });
-}
-
-module.exports.resolveSchemaPath = function (name) {
-  return path.resolve('.', 'examples', name);
 };
 
 module.exports.generate = function (schema, options, callback) {
@@ -98,9 +30,19 @@ module.exports.generate = function (schema, options, callback) {
     }));
 };
 
-// test utility functions
-// module.exports.setUp = setUp;
-// module.exports.tearDown = tearDown;
-// general utilities
-module.exports.sampleAndStrip = sampleAndStrip;
-module.exports.regex = regex;
+module.exports.resolveSchemaPath = function (name) {
+  return path.resolve('.', 'examples', name);
+};
+
+module.exports.sampleAndStrip = function (array, count, fn) {
+  var sample = chance.pick(array, count);
+  async.each(sample,
+    function (item, callback) {
+      item._id = undefined;
+      callback();
+    },
+    function (err) {
+      if (err) throw err;
+      fn(sample);
+  });
+};
