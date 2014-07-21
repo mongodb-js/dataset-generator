84a763823bd6be1aa17d0e2b1d35fd864411bf8b
Tianhao Zhang <tianhao.zhang.2016@gmail.com>
2014-07-21 15:55:11 -0400

    use yargs, full detail help message

--- bin/mongodb-datasets.js
+++ bin/mongodb-datasets.js
@@ -1,25 +1,50 @@
 #!/usr/bin/env node
 
-var md = require('../');
-var helpers = require('../lib/helpers');
-var argv = require('minimist')(process.argv.slice(2));
+var datasets = require('../');
 var es = require('event-stream');
 var fs = require('fs');
+var yargs = require('yargs')
+      .usage('MongoDB-Datasets version 0.0.0')
+      .example('$0 schema.json -n 10', '')
+      .example('cat schema.json | $0 -n 10', '')
+      .describe(0, 'Path to a template schema file')
+      .options('n', {
+        demand: true,
+        alias: 'size',
+        describe: 'Number of documents to generate'
+      })
+      .options('o', {
+        demand: true,
+        alias: 'out',
+        string: true,
+        describe: 'Path to output file. Use "-" for stdout'
+      })
+      .options('pretty', {
+        boolean: true,
+        describe: 'Whether to format results'
+      })
+      .options('h', {
+        alias: 'help',
+        boolean: true,
+        describe: 'Show help message'
+      })
+      .check(function (argv) {
+        if (argv._.length > 1)
+          throw 'Too many arguments!';
+        if (argv._[0] && !fs.existsSync(argv._[0]))
+          throw 'Schema file does not exist!';
+      });
+var argv = yargs.argv;
 
-var myOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.cli);
-var genOpts = helpers.filterOptions(argv, md.DEFAULT_OPTIONS.generator);
+if (argv.h) return yargs.showHelp();
 
-// console.log('All commands: ', argv);
-// console.log('For this script: ', myOpts);
-// console.log('For Generator: ', genOpts);
-
-(myOpts.path ? fs.createReadStream(myOpts.path) : process.stdin)
-  .pipe(md.createGeneratorStream(genOpts))
+(argv._[0] ? fs.createReadStream(argv._[0]) : process.stdin)
+  .pipe(datasets.createGeneratorStream({size: argv.size}))
   .pipe(es.map(function (data, callback) {
-    if (myOpts.raw) {
-      callback(null, JSON.stringify(data));
-    } else {
+    if (argv.pretty) {
       callback(null, JSON.stringify(data, null, 2));
+    } else {
+      callback(null, JSON.stringify(data));
     }
   }))
-  .pipe(process.stdout);
+  .pipe((argv.out === '-') ? process.stdout : fs.createWriteStream(argv.out));
